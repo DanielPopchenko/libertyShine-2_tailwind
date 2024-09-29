f56ad65a179b4308bd7a517805d971fc9501044e
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2024-09-29 12:19:44 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,37 @@ const App = () => {
   // const toggleModal = () => {
   //   setModalOpen((prev) => !prev);
   // };
+
+  // function fizzBuzz(n) {
+  //   let value = n;
+
+  //   if (value % 3 === 0 && value % 5 === 0) {
+  //     console.log('fizzBuzz');
+  //   } else if (value % 3 === 0) {
+  //     console.log('Fizz');
+  //   } else if (value % 5 === 0) {
+  //     console.log('Buzz');
+  //   } else {
+  //     console.log(value);
+  //   }
+  // }
+
+  // fizzBuzz(2);
+
+  function getMaximumEvenSum(val) {
+    let sum = 0;
+    for (let i = 0; i < val.length; i++) {
+      sum += val[i];
+    }
+
+    if (sum % 2 === 0) {
+      return sum;
+    }
+    return null;
+  }
+
+  console.log(getMaximumEvenSum([2, 3, 6, 10, 1, 3]));
+
   return (
     <>
       <Routes>
@@ -21,7 +52,7 @@ const App = () => {
           <Route path="services" element={<ServicesPage />} />
           <Route path="portfolio" element={<Portfolio />} />
           <Route path="questions" element={<Questions />} />
-          <Route path="*" element={<NotFound />} /> // ! Styles
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
 
